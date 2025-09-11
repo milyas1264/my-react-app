43f31340d10e1088098357337cfa8e2b429f4bbe
Muhammad Ilyas <milyasather42401@gmail.com>
2025-09-11 12:55:00 +0500

    update project

--- src/Pages/Counter.jsx
+++ src/Pages/Counter.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
   return (
-    <div style={{marginLeft:"40%",backgroundColor:"blue", textAlign:"center",marginBottom:"10px"}}>
+    <div style={{marginLeft:"40%",backgroundColor:"pink", textAlign:"center",marginBottom:"10px", border:"5px ridge red"}}>
       <h1>Counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}  style={{ marginLeft: "10px" }}>Increase</button>
+      <button onClick={() => setCount(count - 1)}  style={{ marginLeft: "10px" }}>Decrease</button>
+      <button onClick={() => setCount(0)}  style={{ marginLeft: "10px" }}>Reset</button>
       
     </div>
   );
